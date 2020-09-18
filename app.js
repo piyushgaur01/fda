c3c64d2a4588508c19d9d2eaf4a5ee20fb556532
Piyush Gaur <Piyush.Gaur@poly.com>
2020-09-18 12:07:18 +0530

    Incorrect TR using expected return

--- app.js
+++ app.js
@@ -9,13 +9,17 @@ const {
   leastSquareRegression,
 } = require('./library');
 
-const RISK_FREE_RETURN = 0.06;
-const EXPECTED_MARKET_RETURN = 0.1;
-let MARKET_RETURN;
+const MARKET_DATA = [];
 
 const filePath = `${__dirname}\\data\\securities`;
-const market = [];
-const finalData = [];
+
+const constants = {
+  MARKET_RETURN: 0.0,
+  SIGMA_M: 0.0,
+  RISK_FREE_RETURN: 0.06,
+};
+
+let finalData = [];
 
 let adjCloseMarket;
 
@@ -25,22 +29,45 @@ function calculateStockParameters(filename, cb) {
   const { adjclose } = data.indicators.adjclose[0];
 
   const avgReturn = averageReturn(adjclose);
-  const stdDevDailyReturn = standardDeviation(adjclose);
+  const sigmaRi = standardDeviation(adjclose);
 
-  // const slope = leastSquareRegression(adjclose, adjCloseMarket);
   const slope = leastSquareRegression(adjCloseMarket, adjclose);
   const adjBeta = 0.67 * slope + 0.33;
 
-  const treynorRatio = (avgReturn - RISK_FREE_RETURN) / adjBeta;
+  const expectedReturn = constants.RISK_FREE_RETURN + (adjBeta * (constants.MARKET_RETURN - constants.RISK_FREE_RETURN));
+
+  const treynorRatio = (expectedReturn - constants.RISK_FREE_RETURN) / adjBeta;
+
+  const sigmaEpsilon = Math.sqrt((sigmaRi ** 2) - ((adjBeta ** 2) * (constants.SIGMA_M ** 2)));
+
+  // Calculating Ci
+  let Ci;
+  if (finalData.length > 0) {
+    let summNumerator = 0;
+    let summDenominator = 0;
+    finalData.forEach((stock) => {
+      summNumerator += ((stock['Expected Return E(Ri)'] - constants.RISK_FREE_RETURN) * stock['Adj Beta']) / (stock['Sigma Epsilon'] ** 2);
+      summDenominator += (stock['Adj Beta'] ** 2) / (stock['Sigma Epsilon'] ** 2);
+    });
+    Ci = ((constants.SIGMA_M ** 2) * summNumerator) / (1 + (constants.SIGMA_M ** 2) * summDenominator);
+  } else {
+    // for first security in the array
+    const numerator = ((constants.SIGMA_M ** 2) * (((expectedReturn - constants.RISK_FREE_RETURN) * adjBeta) / (sigmaEpsilon ** 2)));
+    const denominator = (1 + (constants.SIGMA_M ** 2) * (adjBeta ** 2 / (sigmaEpsilon ** 2)));
+    Ci = numerator / denominator;
+  }
 
   const security = {
     // name: stock.name,
     code,
     'Average Return': avgReturn,
-    'Std Dev': stdDevDailyReturn,
+    'Std Dev (Sigma_Ri)': sigmaRi,
     'Raw Beta': slope,
     'Adj Beta': adjBeta,
-    TR: treynorRatio,
+    'Expected Return E(Ri)': expectedReturn,
+    'Treynor Ratio (TR)': treynorRatio,
+    'Sigma Epsilon': sigmaEpsilon,
+    Ci,
   };
 
   finalData.push(security);
@@ -61,9 +88,13 @@ function start() {
         }
 
         try {
+          // Filter out the stocks which have a negative beta
+          finalData = finalData.filter((stock) => stock['Adj Beta'] > 0);
+          // Sorting the data in decreasing order based on Treynor's Ratio
+          finalData.sort((a, b) => (a.TR > b.TR ? -1 : 1));
           const opts = { ...Object.keys(finalData[0]) };
           const csvData = parse(finalData, opts);
-          fs.writeFileSync(`${__dirname}\\data\\final-data.csv`, csvData);
+          fs.writeFileSync(`${__dirname}\\data\\final-data-${Date.now()}.csv`, csvData);
         } catch (err2) {
           console.error(err2);
         }
@@ -75,12 +106,11 @@ function start() {
 
 fs.createReadStream(`${__dirname}\\data\\market.csv`)
   .pipe(csv())
-  .on('data', (data) => market.push(data))
+  .on('data', (data) => MARKET_DATA.push(data))
   .on('end', () => {
-    adjCloseMarket = market.map((item) => parseFloat(item['Adj Close']));
-    MARKET_RETURN = averageReturn(adjCloseMarket);
-    const stdDevMarket = standardDeviation(adjCloseMarket);
-    console.log(`Market Return: ${MARKET_RETURN}`);
-    console.log(`Market Std Dev: ${stdDevMarket}`);
+    adjCloseMarket = MARKET_DATA.map((item) => parseFloat(item['Adj Close']));
+    constants.MARKET_RETURN = averageReturn(adjCloseMarket);
+    constants.SIGMA_M = standardDeviation(adjCloseMarket);
+    console.table(constants);
     start();
   });
